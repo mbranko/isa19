a2b6cd5a085b5bf9e0ac3a3aab1f780fad7f5fe3
Branko Milosavljevic <mbranko@uns.ac.rs>
2020-03-18 13:27:54 +0100

    Sitne izmene

--- 04-di/pr01/src/main/java/pr01/MyApp.java
+++ 04-di/pr01/src/main/java/pr01/MyApp.java
@@ -12,14 +12,14 @@ public class MyApp {
   //   this.service = svc;
   // }
 	    
-	//setter method injector
-	@Inject
-	public void setService(MessageService svc){
-		this.service = svc;
-	}
-	
-	public boolean sendMessage(String msg, String rec){
+  //setter method injector
+  @Inject
+  public void setService(MessageService svc){
+  	this.service = svc;
+  }
+
+  public boolean sendMessage(String msg, String rec){
     // some business logic here
-		return service.sendMessage(msg, rec);
-	}
+  	return service.sendMessage(msg, rec);
+  }
 }
